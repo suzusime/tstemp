3c9a46c3c6688f868b3b76a44ccd62c572666991
suzusime <tamagawajousuiro+git@gmail.com>
2018-06-21 21:12:58 +0900

    最小構成

---
+++ src/greeter.ts
@@ -0,0 +1,8 @@
+export interface Person {
+    firstName: string;
+    lastName: string;
+};
+
+export function greeter(person: Person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+}
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import {greeter} from "./greeter";
+
+let user = { firstName: "Migiri", lastName: "Sakihana"};
+
+let sss = greeter(user);
+
+console.log(sss);
